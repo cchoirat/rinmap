56f026ab70368d03ef7b298f30ad8cc0c214983b
Christine Choirat <cchoirat@gmail.com>
2017-10-14 10:58:09 -0400

    More informative function argument names

--- R/rinmap.R
+++ R/rinmap.R
@@ -1,5 +1,5 @@
-create_input_shapefile <- function(input, path) {
-  input <- read.csv(input)
+create_input_shapefile <- function(input_csv, path) {
+  input <- read.csv(input_csv)
   dir.create(path = path, showWarnings = FALSE)
   dir.create(path = file.path(path, "emis"), showWarnings = FALSE)
   proj_inmap <- "+proj=lcc +lat_1=33 +lat_2=45 +lat_0=40 +lon_0=-97 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"
@@ -15,25 +15,21 @@ setup_files_inmap_run <- function(path) {
   dir.create(path = file.path(path, "emis"), showWarnings = FALSE)
 }
 
-zctashape <- "~/Downloads/cb_2015_us_zcta510_500k/cb_2015_us_zcta510_500k.shp"
-shpout <- "output/ptegu.shp"
-crosswalk <- "Crosswalk/Zip_to_ZCTA_crosswalk_2015_JSI.csv"
-
-zip_code_linkage <- function(shpout, zctashape, crosswalk) {
-  zcta <- shapefile(zctashape)
-  result <- shapefile(shpout)
-  pm25 <- result[, 6]
+zip_code_linkage <- function(output_shapefile, zcta_shapefile, crosswalk_csv) {
+  zcta <- shapefile(zcta_shapefile)
+  result <- shapefile(output_shapefile)
+  pm25 <- result[, 6] # subset of PM2.5 only
   zcta2 <- spTransform(zcta, proj4string(pm25))
   o <- over(zcta2, pm25, fn = mean, na.rm = TRUE)
   O <- cbind(zcta@data[["ZCTA5CE10"]], o)
   names(O) <- c("ZCTA", "PM25inMAP")
-  O$ZCTA <- as.numeric(O$ZCTA)
-  crosswalk <- read.csv(crosswalk)
+  O$ZCTA <- as.numeric(O$ZCTA) # to merge on zcta ID
+  crosswalk <- read.csv(crosswalk_csv)
   M <- merge(O, crosswalk, by = "ZCTA")
   return(M)
 }
 
-rinmap <- function(input, output, zctashape, crosswalk) {
+rinmap <- function(input_csv, output_path, zcta_shapefile, crosswalk_csv) {
   # TODO
   return()
 }
