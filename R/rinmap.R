1a18ddb2ab65f2322bc228da04b2b31c5de4399d
Christine Choirat <cchoirat@gmail.com>
2017-10-13 22:12:55 -0400

    Setup files ready to be copied

--- R/rinmap.R
+++ R/rinmap.R
@@ -1,25 +1,26 @@
-create_input_shapefile <- function(input, shpout = "emis") {
-  emis <- read.csv(input)
+create_input_shapefile <- function(input, path, shpout = "emis") {
+  input <- read.csv(input)
+  dir.create(path = path, showWarnings = FALSE)
+  dir.create(path = file.path(path, shpout), showWarnings = FALSE)
   proj_inmap <- "+proj=lcc +lat_1=33 +lat_2=45 +lat_0=40 +lon_0=-97 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"
-  spemis <- copy(emis)
-  coordinates(spemis) <- ~Longitude + Latitude
-  crs(spemis) <- CRS("+proj=longlat +datum=NAD83")
-  spemis <- spTransform(spampd, proj_inmap)
-  shapefile(spemis, file.path(shpout, "ptegu.shp"))
-  return()
+  coordinates(input) <- ~Longitude + Latitude
+  crs(input) <- CRS("+proj=longlat +datum=NAD83")
+  input <- spTransform(input, proj_inmap)
+  shapefile(input, file.path(path, shpout, "ptegu.shp"))
 }
 
-run_inmap <- function(input, output, save) {
+setup_files_inmap_run <- function(path, shpout = "emis") {
+  file.copy(from = system.file("inmap.toml", package = "rinmap"), to = path, overwrite = TRUE)
+  file.copy(from = system.file("run.sh", package = "rinmap"), to = path, overwrite = TRUE)
+  dir.create(path = file.path(path, shpout), showWarnings = FALSE)
+}
+
+zip_code_linkage <- function(output, zctashape, crosswalk) {
   # TODO
   return()
 }
 
-zip_code_linkage <- function(output, save, zctashape, crosswalk) {
-  # TODO
-  return()
-}
-
-rinmap <- function(input, output, save, zctashape, crosswalk) {
+rinmap <- function(input, output, zctashape, crosswalk) {
   # TODO
   return()
 }
